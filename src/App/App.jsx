3a9fee0ef160335ea4b9cbed2f1a958f027f3428
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-04-16 13:38:58 +0530

    Made the Navbar in all the pages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,17 +4,19 @@ import Home from '../Components/Home';
 import Error from '../Components/Error';
 import MovieDetail from '../Components/MovieDetail/MovieDetail';
 import { useGlobalRoutePathObject } from '../Helper/PathHelper';
+import NavbarComponent from '../Components/NavbarComponent';
 
 
 const App = () => {
 
   return (
     <section id="app-rc">
-      <Routes>
-        <Route path={useGlobalRoutePathObject.Home} element={<Home />} />
-        <Route path={useGlobalRoutePathObject.MovieDetails} element={<MovieDetail />} />
-        <Route path={useGlobalRoutePathObject.OtherPaths} element={<Error />} />
-      </Routes>
+      <NavbarComponent/>
+        <Routes>
+          <Route path={useGlobalRoutePathObject.Home} element={<Home />} />
+          <Route path={useGlobalRoutePathObject.MovieDetails} element={<MovieDetail />} />
+          <Route path={useGlobalRoutePathObject.OtherPaths} element={<Error />} />
+        </Routes>
     </section>
   )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Search from './Search';
 import Movies from './Movies';
-import NavbarComponent from './NavbarComponent';
 
 const Home = () => {
 
   return (
     <div>
-      <NavbarComponent />
       <Search />
       <Movies />
     </div>
